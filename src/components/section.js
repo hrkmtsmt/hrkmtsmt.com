8f2e73326c2627a53891371763dd50debbb7e181
hrkmtsmt <hrkmtsmt@icloud.com>
2021-07-24 23:53:45 +0900

    :bug: bug fix: uninstall react-reveal

--- src/components/section.js
+++ src/components/section.js
@@ -1,17 +1,14 @@
 import React from "react";
-import Fade from "react-reveal/Fade";
 export const Section = (props) => {
     return (
-        <Fade bottom delay={400}>
-            <section className={"l-grid l-inner"}>
-                <div className={"l-grid-full"}>
-                    <div className={"c-section"}>
-                        <h2 className={"c-section-title"}>{props.title}</h2>
-                        <p className={"c-section-desc"}>{props.copy}</p>
-                    </div>
+        <section className={"l-grid l-inner"}>
+            <div className={"l-grid-full"}>
+                <div className={"c-section"}>
+                    <h2 className={"c-section-title"}>{props.title}</h2>
+                    <p className={"c-section-desc"}>{props.copy}</p>
                 </div>
-                {props.children}
-            </section>
-        </Fade>
+            </div>
+            {props.children}
+        </section>
     );
 };
