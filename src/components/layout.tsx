6a992f1fe63a04513735a7ef959c9617fc182b27
hrkmtsmt <hrkmtsmt@icloud.com>
2021-08-09 03:14:50 +0900

    :bug: bug fix: test remove Header.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Header } from "./Header";
 import { Footer } from "./Footer";
 type Props = {
   children?: React.ReactNode;
@@ -7,7 +6,6 @@ type Props = {
 export const Layout = (props: Props) => {
   return (
     <React.StrictMode>
-      <Header />
       <main className={"l-main"}>{props.children}</main>
       <Footer />
     </React.StrictMode>
