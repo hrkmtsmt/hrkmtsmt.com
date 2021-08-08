d4453bda38d7d3c6f99e18f287eebe4fa2bee0f8
hrkmtsmt <hrkmtsmt@icloud.com>
2021-08-09 03:18:46 +0900

    :bug: bug fix: rename file header.tsx footer.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Footer } from "./Footer";
+import { Header } from "./header";
+import { Footer } from "./footer";
 type Props = {
   children?: React.ReactNode;
 };
 export const Layout = (props: Props) => {
   return (
     <React.StrictMode>
+      <Header />
       <main className={"l-main"}>{props.children}</main>
       <Footer />
     </React.StrictMode>
