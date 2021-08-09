e5475c65f8ad3c4b9181469f6949919dedd13c7f
hrkmtsmt <hrkmtsmt@icloud.com>
2021-08-09 16:35:39 +0900

    :+1: feature: new hamburger

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,31 +3,31 @@ import Link from "next/link";
 import { LinkButton } from "./link-button";
 export const Header = () => {
   useEffect(() => {
+    const slideNav = document.getElementById("slide-nav")!;
+    const hamburgerButton = document.getElementById("hamburger-button")!;
+    const slideNavItem = document.querySelectorAll(".c-slide-nav-item")!;
+    const mediaQuery = matchMedia("(max-width: 768px)").matches;
     addEventListener("scroll", function () {
       const scroll = window.pageYOffset;
-      const header = this.document.getElementById("header")!;
-      if (scroll > 400) {
-        header.classList.add("is-scroll")!;
+      if (mediaQuery || scroll > 160) {
+        hamburgerButton.classList.add("is-scroll")!;
       } else {
-        header.classList.remove("is-scroll")!;
+        hamburgerButton.classList.remove("is-scroll")!;
       }
     });
-    const mobileNav = document.getElementById("mobile-nav")!;
-    const hamburgerButton = document.getElementById("hamburger-button")!;
-    const mobileNavItem = document.querySelectorAll(".c-mobile-nav-item")!;
     hamburgerButton.addEventListener("click", () => {
-      mobileNav.classList.toggle("is-active");
-      mobileNavItem.forEach((item, index) => {
+      slideNav.classList.toggle("is-active");
+      slideNavItem.forEach((item, index) => {
         const order = index + 1;
         const delay = order * 200;
         setTimeout(() => {
           item.classList.toggle("is-order");
         }, delay);
       });
-      const mobileNavIsActive = mobileNav.classList.contains("is-active")!;
+      const slideNavIsActive = slideNav.classList.contains("is-active")!;
       const hamburgerOpen = document.getElementById("hamburger-open")!;
       const hamburgerClose = document.getElementById("hamburger-close")!;
-      if (mobileNavIsActive === false) {
+      if (slideNavIsActive === false) {
         hamburgerOpen.classList.add("is-visible");
         hamburgerClose.classList.remove("is-visible");
       } else {
@@ -45,46 +45,46 @@ export const Header = () => {
           </Link>
         </h1>
         <nav className={"c-header-nav"}>
-          <div className={"c-header-nav-desktop"}>
-            <ul className={"c-nav-horizonal"}>
-              <li className={"c-nav-item"}>
-                <Link href={"sdf"}>
-                  <a className={"c-nav-link"}>aaa</a>
-                </Link>
-              </li>
-            </ul>
-            <LinkButton link={"/"} cta={"a"} />
-          </div>
-          <div className={"c-header-nav-mobile"}>
-            <button id={"hamburger-button"} className={"c-hamburger-button"}>
-              <i
-                id={"hamburger-open"}
-                className={"c-hamburger-button-icon is-visible ci-menu_alt_01"}
-              ></i>
-              <i
-                id={"hamburger-close"}
-                className={"c-hamburger-button-icon ci-close_big"}
-              ></i>
-            </button>
-            <ul id={"mobile-nav"} className={"c-mobile-nav"}>
-              <li className={"c-mobile-nav-item"}>
-                <Link href={"sdf"}>
-                  <a className={"c-mobile-nav-link"}>aaa</a>
-                </Link>
-              </li>
-              <li className={"c-mobile-nav-item"}>
-                <Link href={"sdf"}>
-                  <a className={"c-mobile-nav-link"}>aaa</a>
-                </Link>
-              </li>
-              <li className={"c-mobile-nav-item"}>
-                <Link href={"sdf"}>
-                  <a className={"c-mobile-nav-link"}>aaa</a>
-                </Link>
-              </li>
-            </ul>
-          </div>
+          <ul className={"c-nav-horizonal"}>
+            <li className={"c-nav-item"}>
+              <Link href={"sdf"}>
+                <a className={"c-nav-link"}>aaa</a>
+              </Link>
+            </li>
+            <li className={"c-nav-item"}>
+              <LinkButton link={"/"} cta={"a"} />
+            </li>
+          </ul>
         </nav>
+        <nav id={"slide-nav"} className={"c-slide-nav"}>
+          <ul className={"c-slide-nav-inner"}>
+            <li className={"c-slide-nav-item"}>
+              <Link href={"sdf"}>
+                <a className={"c-slide-nav-link"}>aaa</a>
+              </Link>
+            </li>
+            <li className={"c-slide-nav-item"}>
+              <Link href={"sdf"}>
+                <a className={"c-slide-nav-link"}>aaa</a>
+              </Link>
+            </li>
+            <li className={"c-slide-nav-item"}>
+              <Link href={"sdf"}>
+                <a className={"c-slide-nav-link"}>aaa</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <button id={"hamburger-button"} className={"c-hamburger-button"}>
+          <i
+            id={"hamburger-open"}
+            className={"c-hamburger-button-icon is-visible ci-menu_alt_01"}
+          ></i>
+          <i
+            id={"hamburger-close"}
+            className={"c-hamburger-button-icon ci-close_big"}
+          ></i>
+        </button>
       </div>
     </header>
   );
