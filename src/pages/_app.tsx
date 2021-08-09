7ac395bee85099d8bb606a3258b0d5c7c23803f2
hrkmtsmt <hrkmtsmt@icloud.com>
2021-08-09 11:15:53 +0900

    :+1: feature: new feature loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
+import React from "react";
 import "../styles/reset.css";
 import "../styles/coolicons.css";
 import "../styles/style.scss";
 import "swiper/swiper.scss";
 import { AppProps } from "next/app";
-import React from "react";
+import { Layout } from "../components/layout";
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <React.Fragment>
+      <Layout />
       <Component {...pageProps} />
     </React.Fragment>
   );
