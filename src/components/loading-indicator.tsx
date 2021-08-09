3b48891b68295012b9b2eb5cbd3329135522a9ff
hrkmtsmt <hrkmtsmt@icloud.com>
2021-08-09 12:01:19 +0900

    :bug: bug fix: loading-indicator.tsx change event

--- src/components/loading-indicator.tsx
+++ src/components/loading-indicator.tsx
@@ -2,9 +2,9 @@ import React, { useEffect } from "react";
 export const LoadingIndicator = () => {
   useEffect(() => {
     const loader = document.getElementById("loader")!;
-    onload = () => {
+    addEventListener("DOMContentLoaded", () => {
       loader.classList.add("is-loaded");
-    };
+    });
   }, []);
   return (
     <div id={"loader"} className={"c-loader"}>
