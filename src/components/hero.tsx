bcfabfb2d2497f01001fbc6cddce980eff0c343e
hrkmtsmt <hrkmtsmt@icloud.com>
2021-09-02 23:40:06 +0900

    :art: ui/ux: fix slide-nav.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,13 +4,9 @@ export const Hero = () => {
   return (
     <div className={"l-grid-full p-hero"}>
       <div className={"l-grid"}>
-        <div className={"l-grid-large"}>
-          <div className={"aaa "}>
-            React, JavaScript, CSS, SCSS, HTML, TypeScipt
-          </div>
-          <div className={"bbb"}>
-            WordPress, PHP, Adobe XD, Illustrator, Photoshop, something…
-          </div>
+        <div className={"l-grid-full"}>
+          <div className={"aaa "}>Front End is</div>
+          <div className={"bbb"}>aaa</div>
         </div>
         <div className={"l-grid-large aaa"}></div>
         <div className={"l-grid-full"}>
