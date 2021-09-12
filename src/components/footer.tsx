6501306e91b528ec6339535e2743277d821ad1d6
hrkmtsmt <hrkmtsmt@icloud.com>
2021-09-13 03:26:26 +0900

    :art: ui/ux: use social icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,28 +6,42 @@ export const Footer = () => {
         <nav className={"l-footer-nav"}>
           <ul className={"c-nav-horizonal"}>
             <li className={"c-nav-item"}>
-              <a href={""} className={"c-nav-link"}>
-                <i className={"c-nav-icon ci-twitter"}></i>
+              <a href={"https://twitter.com/hrkmtsmt"} className={"c-nav-link"}>
+                <i className={"c-nav-icon i-twitter"}></i>
+              </a>
+            </li>
+            <li className={"c-nav-item"}>
+              <a href={"https://zenn.dev/hrkmtsmt"} className={"c-nav-link"}>
+                <i className={"c-nav-icon i-zenn"}></i>
+              </a>
+            </li>
+            <li className={"c-nav-item"}>
+              <a
+                href={"https://www.instagram.com/hrkmtsmt/"}
+                className={"c-nav-link"}
+              >
+                <i className={"c-nav-icon i-instagram"}></i>
+              </a>
+            </li>
+            <li className={"c-nav-item"}>
+              <a
+                href={"https://www.behance.net/hrkmtsmt"}
+                className={"c-nav-link"}
+              >
+                <i className={"c-nav-icon i-behance"}></i>
               </a>
             </li>
             <li className={"c-nav-item"}>
               <a href={""} className={"c-nav-link"}>
-                <i className={"c-nav-icon ci-instagram"}></i>
+                <i className={"c-nav-icon i-dribbble"}></i>
               </a>
             </li>
             <li className={"c-nav-item"}>
-              <a href={""} className={"c-nav-link"}>
-                <i className={"c-nav-icon ci-behance"}></i>
-              </a>
-            </li>
-            <li className={"c-nav-item"}>
-              <a href={""} className={"c-nav-link"}>
-                <i className={"c-nav-icon ci-dribbble"}></i>
-              </a>
-            </li>
-            <li className={"c-nav-item"}>
-              <a href={""} className={"c-nav-link"}>
-                <i className={"c-nav-icon ci-github"}></i>
+              <a
+                href={"https://dribbble.com/hrkmtsmt"}
+                className={"c-nav-link"}
+              >
+                <i className={"c-nav-icon i-github"}></i>
               </a>
             </li>
           </ul>
