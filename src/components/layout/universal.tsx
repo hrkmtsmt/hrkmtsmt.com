704d61be777676f7db1acf10e4312aacf88f6dfe
hrkmtsmt <hrkmtsmt@icloud.com>
2021-09-30 14:22:39 +0900

    :bug: bug fix: remove src dir for bug fix

--- src/components/layout/universal.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-type Props = {
-  children?: React.ReactNode;
-};
-export const Universal = (props: Props) => {
-  return <div className={"l-universal"}>{props.children}</div>;
-};
--- src/config/environment-variable.tsx
+++
@@ -1,4 +0,0 @@
-export const ENDPOINT = process.env.ENDPOINT;
-export const API_KEY: {} = {
-  headers: { "X-API-KEY": process.env.API_KEY },
-};
