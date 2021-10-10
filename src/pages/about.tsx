a7f6853c8afa3dc30480378ec9c2b16b65550041
hrkmtsmt <hrkmtsmt@icloud.com>
2021-10-10 21:26:32 +0900

    :art: ui/ux: create ui FeatureBox.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,8 @@ import { Helmet } from "../components/Helmet";
 import { Main } from "../components/layout/Main";
 import { Section } from "../components/layout/Section";
 import { Layout } from "../components/layout/Layout";
-import { Medium } from "../components/layout/Grid";
+import { Medium, Large } from "../components/layout/Grid";
+import { FeatureBox } from "../components/FeatureBox";
 import { ENDPOINT, API_KEY } from "../config/environment-variable";
 type Props = {
   intro: [];
@@ -18,13 +19,12 @@ export default function About({ intro }: Props) {
         <Section id={"introduction"} title={"Introduction"}>
           {intro.map((intro: any) => (
             <Medium key={intro.title}>
-              <div className={"c-feature-box"}>
-                <div className={"c-feature-box-emoji"}>{intro.emoji}</div>
-                <h3 className={"c-feature-box-title"}>{intro.title}</h3>
-                <div className={"c-feature-box-desc"}>{intro.desc}</div>
-              </div>
+              <FeatureBox title={intro.title} desc={intro.desc} image={intro.image} />
             </Medium>
           ))}
+        </Section>
+        <Section id={"q-and-a"} title={"Q&A"}>
+          <Large>a</Large>
         </Section>
       </Main>
     </Layout>
