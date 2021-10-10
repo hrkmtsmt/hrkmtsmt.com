d22944bbd21ae47b1ab5647e35a81e994b337e36
hrkmtsmt <hrkmtsmt@icloud.com>
2021-10-10 23:59:09 +0900

    :art: ui/ux: create ui Accordion.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Helmet } from "../components/Helmet";
 import { Main } from "../components/layout/Main";
 import { Section } from "../components/layout/Section";
 import { Layout } from "../components/layout/Layout";
-import { Medium, Large } from "../components/layout/Grid";
+import { Large } from "../components/layout/Grid";
 import { FeatureBox } from "../components/FeatureBox";
+import { Accordion } from "../components/Accordion";
 import { ENDPOINT, API_KEY } from "../config/environment-variable";
 type Props = {
   intro: [];
@@ -17,14 +19,37 @@ export default function About({ intro }: Props) {
       <Helmet title={title} desc={desc} />
       <Main>
         <Section id={"introduction"} title={"Introduction"}>
-          {intro.map((intro: any) => (
-            <Medium key={intro.title}>
-              <FeatureBox title={intro.title} desc={intro.desc} image={intro.image} />
-            </Medium>
-          ))}
+          <Swiper
+            className={"l-grid-full"}
+            pagination={{ clickable: true }}
+            breakpoints={{
+              0: {
+                spaceBetween: 24,
+                slidesPerView: 1.33333,
+              },
+              576: {
+                spaceBetween: 24,
+                slidesPerView: 2,
+              },
+              768: {
+                spaceBetween: 32,
+                slidesPerView: 3,
+              },
+            }}
+          >
+            {intro.map((intro: any, index) => (
+              <SwiperSlide key={index}>
+                <FeatureBox title={intro.title} desc={intro.desc} image={intro.image} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </Section>
         <Section id={"q-and-a"} title={"Q&A"}>
-          <Large>a</Large>
+          {intro.map((intro: any, index) => (
+            <Large key={index}>
+              <Accordion question={intro.title} answer={intro.desc} />
+            </Large>
+          ))}
         </Section>
       </Main>
     </Layout>
