4f029ca738d0feb4f95e62a1aaa074cf409a6f16
hrkmtsmt <hrkmtsmt@icloud.com>
2022-01-11 02:39:31 +0900

    :eyes: [Test]: test

--- src/config/environment-variable.tsx
+++ src/config/environment-variable.tsx
@@ -1,4 +1,4 @@
-export const ENDPOINT = process.env.ENDPOINT;
+export const ENDPOINT: string | undefined = process.env.ENDPOINT;
 export const API_KEY: {} = {
   headers: { 'X-MICROCMS-API-KEY': process.env.API_KEY },
 };
