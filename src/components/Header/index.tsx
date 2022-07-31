7bf5929706aead6415e6d66d504adce3099e5942
hrkmtsmt <hrkmtsmt@icloud.com>
2022-07-31 22:59:38 +0900

    :wrench: [FIX]: Fixed Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,24 +13,24 @@ export type HeaderProps = Props;
 
 const Component: React.FC<Props> = props => {
   return (
-    <header className={'pl-[32px] pr-[32px] flex justify-center'}>
-      <div className={`w-[100%] max-w-screen-xl h-[64px] flex flex-row`}>
-        <nav className={'flex flex-row gap-[16px] items-center'}>
+    <header className={'pl-8 pr-8 flex justify-center'}>
+      <div className={`w-full max-w-screen-xl h-[64px] flex flex-row`}>
+        <nav className={'flex flex-row gap-4 items-center'}>
           <h1>
             <Link href={'/'} passHref>
-              <a className={'h-[40px]'}>
+              <a className={'h-10'}>
                 <Logo size={'40px'} color={'primary'} />
               </a>
             </Link>
           </h1>
-          <ul className={'flex flex-row gap-[16px] items-center'}>
+          <ul className={'flex flex-row gap-4 items-center'}>
             {props.headerMenus.map(menu => {
               return (
                 <li>
                   <Link href={menu.link} passHref>
                     <a
                       className={
-                        'font-qualion-bold relative text-[1.5rem] block leading-[40px] text-primary hover:text-secondary ease-in-out duration-200 hover:ease-in-out hover:duration-200 hover:after:content-[""] after:w-[8px] after:h-[8px] after:bg-primary after:block after:rounded-[8px] after:absolute after:left-[50%] after:-translate-x-[50%] after:-bottom-[16px] after:opacity-0 hover:after:bg-secondary hover:after:opacity-100 after:hover:-bottom-[8px] after:ease-in-out after:duration-200 after:hover:duration-200 after:hover:ease-in-out'
+                        'font-qualion-bold relative text-[24px] block leading-10 text-primary hover:text-secondary ease-in-out duration-200 hover:ease-in-out hover:duration-200 hover:after:content-[""] after:w-2 after:h-2 after:bg-primary after:block after:rounded-2 after:absolute after:left-[50%] after:-translate-x-[50%] after:-bottom-4 after:opacity-0 hover:after:bg-secondary hover:after:opacity-100 after:hover:-bottom-2 after:ease-in-out after:duration-200 after:hover:duration-200 after:hover:ease-in-out'
                       }
                     >
                       {menu.name}
