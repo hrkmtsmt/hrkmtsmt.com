794a010b4649cb962fc3da624ebffbda1d9ce372
hrkmtsmt <hrkmtsmt@icloud.com>
2022-08-01 20:09:21 +0900

    :+1: [FEATURE]: Created Layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Main } from '@src/components/Main';
+import { Header } from '@src/components/Header';
+import { Footer } from '@src/components/Footer';
+import type { MainProps } from '@src/components/Main';
+import type { HeaderProps } from '@src/components/Header';
+import type { FooterProps } from '@src/components/Footer';
+
+type Props = {
+  mainContent: MainProps['children'];
+  headerMenus: HeaderProps['headerMenus'];
+  socialLinks: FooterProps['socialLinks'];
+  copyright: FooterProps['copyright'];
+};
+
+const Component: React.FC<Props> = props => {
+  return (
+    <React.Fragment>
+      <Header headerMenus={props.headerMenus} />
+      <Main>{props.mainContent}</Main>
+      <Footer socialLinks={props.socialLinks} copyright={props.copyright} />
+    </React.Fragment>
+  );
+};
+
+export const Layout = React.memo(Component);
