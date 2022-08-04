0f641731a16ed7e0e2aa6bd9c7f6d10b56804832
hrkmtsmt <hrkmtsmt@icloud.com>
2022-08-05 00:02:31 +0900

    :+1: [FEATURE]: Created custom hook what it fetch article links from rss

---
+++ src/hooks/useFetchArticleLinksFromRSS.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect, useCallback } from 'react';
+import Parser from 'rss-parser';
+
+type Item = {
+  title: string;
+  link: string;
+  creator: string;
+  isoDate: string;
+};
+
+export const useFetchArticleLinksFromRSS = () => {
+  const [articleLinks, setArticleLinks] = useState<Array<Item>>([]);
+  const parser = new Parser();
+
+  useEffect(() => {
+    (async () => {
+      const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/' as const;
+      const url = 'https://zenn.dev/hrkmtsmt/feed';
+      await parser.parseURL(CORS_PROXY + url, (error, feed) => {
+        if (error) throw error;
+        const results = feed.items.map((item) => {
+          return {
+            title: item.title,
+            link: item.link,
+            creator: item.creator,
+            isoDate: item.isoDate
+          };
+        });
+        setArticleLinks(results);
+      });
+    })();
+  }, []);
+
+  return { articleLinks };
+};
