f56df3cafaf0a52e85b846e8d4f1f53940e83780
hrkmtsmt <hrkmtsmt@icloud.com>
2022-08-06 00:12:04 +0900

    :wrench: [FIX]: Fixed PostCard component

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import Link from 'next/link';
 import { Button } from '@src/components/Button';
-import { UrlObject } from 'url';
 
 type Props = {
   title: string;
@@ -10,20 +10,23 @@ type Props = {
 
 const Component: React.VFC<Props> = (props) => {
   return (
-    <a
-      href={props.href}
-      className={
-        'w-full grid-col-1 grid gap-4 rounded-[16px] bg-base p-4 leading-6 duration-200 ease-in-out hover:drop-shadow-regular hover:duration-200 hover:ease-in-out'
-      }
-    >
-      <div>
-        <div className={'text-[12px] text-primary font-bold'}>{props.tag}</div>
-        <h3 className={'text-4 font-bold h-12 overflow-hidden'}>
-          {props.title}
-        </h3>
-      </div>
-      <Button color={'secondary'}>More</Button>
-    </a>
+    <Link href={props.href} passHref>
+      <a
+        className={
+          'w-full grid-col-1 grid gap-4 rounded-[16px] bg-base p-4 leading-6 duration-200 ease-in-out hover:drop-shadow-regular hover:duration-200 hover:ease-in-out'
+        }
+      >
+        <div>
+          <div className={'text-[12px] text-primary font-bold'}>
+            {props.tag}
+          </div>
+          <h3 className={'text-4 font-bold h-12 overflow-hidden'}>
+            {props.title}
+          </h3>
+        </div>
+        <Button color={'secondary'}>More</Button>
+      </a>
+    </Link>
   );
 };
 
