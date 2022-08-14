01c2ffe73b99d4f7a953710c95955301fe4ab39c
hrkmtsmt <hrkmtsmt@icloud.com>
2022-08-14 18:48:06 +0900

    :+1: [FEATURE]: Created Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,65 @@
 import React from 'react';
+import { GitHub, Instagram, Twitter } from 'react-feather';
+import { Layout } from '@src/components/Layout';
+import { Grid } from '@src/components/Grid';
+import { PostCard } from '@src/components/PostCard';
+import { Section } from '@src/components/Section';
+import { useFetchArticleLinksFromRSS } from '@src/hooks/useFetchArticleLinksFromRSS';
+import type { HeaderProps } from '@src/components/Header';
+import type { FooterProps } from '@src/components/Footer';
 
 const Index = () => {
-  return <div className={'bg-slate-300'}>Hello World</div>;
+  const { articleLinks } = useFetchArticleLinksFromRSS();
+
+  const headerMenus: HeaderProps['headerMenus'] = [
+    {
+      name: 'About',
+      link: '/about'
+    }
+  ];
+
+  const socialLinks: FooterProps['socialLinks'] = [
+    {
+      name: 'github',
+      link: 'https://github.com/hrkmtsmt',
+      children: <GitHub size={16} strokeWidth={2.4} />
+    },
+    {
+      name: 'twitter',
+      link: 'https://twitter.com/hrkmtsmt',
+      children: <Twitter size={16} strokeWidth={2.4} />
+    },
+    {
+      name: 'instagram',
+      link: 'https://instagram.com/hrkmtsmt',
+      children: <Instagram size={16} strokeWidth={2.4} />
+    }
+  ];
+
+  return (
+    <Layout
+      headerMenus={headerMenus}
+      mainContent={
+        <Section title={'Articles'}>
+          <Grid>
+            {articleLinks.map((articleLink) => {
+              return (
+                <Grid.Column key={articleLink.link} size={'medium'}>
+                  <PostCard
+                    tag={'Zenn'}
+                    title={articleLink.title}
+                    href={articleLink.link}
+                  />
+                </Grid.Column>
+              );
+            })}
+          </Grid>
+        </Section>
+      }
+      socialLinks={socialLinks}
+      copyright={'hrkmtsmt'}
+    />
+  );
 };
 
 export default Index;
