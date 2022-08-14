cd2191dca4b9e27de3a80c4e7490ee88bdbcc692
hrkmtsmt <hrkmtsmt@icloud.com>
2022-08-14 19:40:53 +0900

    :wrench: [FIX]: Fixed font load settings

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => {
   return (
     <Html lang={'ja'}>
-      <Head />
+      <Head>
+        <link
+          rel='preload'
+          href='/fonts/qualion-round.woff'
+          as='font'
+          type='font/woff'
+          crossOrigin='anonymous'
+        />
+        <link
+          rel='preload'
+          href='/fonts/qualion-round.woff2'
+          as='font'
+          type='font/woff2'
+          crossOrigin='anonymous'
+        />
+      </Head>
       <body
         className={
           'bg-white font-qualion text-font-primary-light duration-[0.4s] ease-in-out dark:bg-black dark:text-font-primary-dark '
