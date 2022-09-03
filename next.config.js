16e6fa8091db5ec74ea184b4e91c30b5b8f892ab
hrkmtsmt <hrkmtsmt@icloud.com>
2022-09-03 13:20:33 +0900

    :wrench: [FIX]: Fixed config file and installed swr

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL
+  },
+  pageExtensions: [
+    'page.tsx',
+    'page.ts',
+    'page.jsx',
+    'page.js',
+    'api.tsx',
+    'api.ts',
+    'api.jsx',
+    'api.js'
+  ]
+};
+
+module.exports = nextConfig;
