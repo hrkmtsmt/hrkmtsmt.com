592b2eb0f5fdb3a3983d3664e69276d88cd092b8
hrkmtsmt <hrkmtsmt@icloud.com>
2022-10-29 14:49:31 +0900

    :wrench: [FIX]: Installed and setted eslint-plugin-css-modules

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { ThemeProvider } from '@src/components/ThemeProvider';
 import { store } from '@src/store';
 import 'tailwindcss/tailwind.css';
-import '@src/styles/globals.css';
+import '@src/styles/global.css';
 import '@src/styles/styles.global.scss';
 
 const App = ({ Component, pageProps }: AppProps) => {
