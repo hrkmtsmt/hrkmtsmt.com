591379b02c5fe8ff0ed1a3500a0e6e2b3813eced
hrkmtsmt <hrkmtsmt@icloud.com>
2022-11-27 01:52:37 +0900

    :+1: [FEATURE]: Implemented basic auth

---
+++ src/middleware.api.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import { PATHS } from './constants/paths';
+import type { NextMiddleware, NextRequest } from 'next/server';
+
+export const config = {
+  matcher: ['/((?!api|_next/static|favicon.ico).*)']
+};
+
+const middleware: NextMiddleware = (request: NextRequest) => {
+  if (process.env.NODE_ENV === 'production') return;
+
+  const authHeader = request.headers.get('authorization');
+  const url = request.nextUrl;
+
+  if (authHeader) {
+    const basicAuth = authHeader.split(' ')[1];
+    const [user, password] = atob(basicAuth).split(':');
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      password === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next();
+    }
+  }
+
+  url.pathname = PATHS.APIS.AUTH.URL;
+
+  return NextResponse.rewrite(url);
+};
+
+export default middleware;
